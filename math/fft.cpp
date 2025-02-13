a073ef9ae2524cd0303b197376ba0495062ece11
nasif03 <121179152+nasif03@users.noreply.github.com>
2025-02-13 22:03:20 +0600

    Add FFT

---
+++ math/fft.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// tested on https://atcoder.jp/contests/abc392/tasks/abc392_g
+
+const int MAX = 1000006;
+using cd = complex<double>;
+const double PI = acos(-1);
+
+
+void fft(vector<cd> & a, bool invert) {
+    int n = a.size();
+    if (n == 1)
+        return;
+
+    vector<cd> a0(n / 2), a1(n / 2);
+    for (int i = 0; 2 * i < n; i++) {
+        a0[i] = a[2*i];
+        a1[i] = a[2*i+1];
+    }
+    fft(a0, invert);
+    fft(a1, invert);
+
+    double ang = 2 * PI / n * (invert ? -1 : 1);
+    cd w(1), wn(cos(ang), sin(ang));
+    for (int i = 0; 2 * i < n; i++) {
+        a[i] = a0[i] + w * a1[i];
+        a[i + n/2] = a0[i] - w * a1[i];
+        if (invert) {
+            a[i] /= 2;
+            a[i + n/2] /= 2;
+        }
+        w *= wn;
+    }
+}
+
+// function for multiplying two polynomials
+vector<int> multiply(vector<int> const& a, vector<int> const& b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<int> result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    return result;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    vector<int> p(MAX);
+    for (int i = 1; i <= n; i++) {
+        int x; cin >> x;
+        p[x] = 1;
+    }
+
+    vector<int> c = multiply(p, p);
+
+    int64_t ans = 0;
+    for (int i = 1; 2 * i < c.size(); i++) {
+        if (p[i]) ans += (c[2 * i] - 1) / 2;
+    }
+    cout << ans;
+}
