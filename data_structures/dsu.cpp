02b1c53c5c61efad4e17be3e65f602a6adb45650
nasif03 <121179152+nasif03@users.noreply.github.com>
2025-06-26 19:09:56 +0600

    Add DSU

--- data_structures/dsu.cpp
+++ data_structures/dsu.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 struct DSU {
-	vector<int> p;
-	DSU(int n) : p(n, -1) {}
-	bool same_set(int a, int b) {
+    vector<int> p;
+    DSU(int n) : p(n, -1) {}
+    bool same_set(int a, int b) {
         return find(a) == find(b);
     }
-	int size(int x) {
+    int size(int x) {
         return -p[find(x)];
     }
-	int find(int x) {
+    int find(int x) {
         return p[x] < 0 ? x : p[x] = find(p[x]);
     }
-	bool join(int a, int b) {
-		a = find(a), b = find(b);
-		if (a == b) return false;
-		if (p[a] > p[b]) swap(a, b);
-		p[a] += p[b];
+    bool join(int a, int b) {
+        a = find(a), b = find(b);
+        if (a == b) return false;
+        if (p[a] > p[b]) swap(a, b);
+        p[a] += p[b];
         p[b] = a;
-		return true;
-	}
+        return true;
+    }
 };
 
 int main() {
