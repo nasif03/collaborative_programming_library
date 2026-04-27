293569a369a5e03761012a5985223c1251ffc166
nasif03 <121179152+nasif03@users.noreply.github.com>
2026-04-27 21:19:29 +0600

    update dsu.cpp

--- data_structures/dsu.cpp
+++ data_structures/dsu.cpp
@@ -1,25 +1,35 @@
+// tested on https://judge.yosupo.jp/problem/unionfind
+
 #include <bits/stdc++.h>
 using namespace std;
 
-struct DSU {
-    vector<int> p;
-    DSU(int n) : p(n, -1) {}
-    bool same_set(int a, int b) {
-        return find(a) == find(b);
+struct dsu {
+    vector<int> p, sz;
+    int n;
+
+    dsu(int _n) : n(_n) {
+        p.resize(n);
+        sz.resize(n);
+        iota(p.begin(), p.end(), 0);
+        fill(sz.begin(), sz.end(), 1);
     }
-    int size(int x) {
-        return -p[find(x)];
-    }
+
     int find(int x) {
-        return p[x] < 0 ? x : p[x] = find(p[x]);
+        return (x == p[x] ? x : (p[x] = find(p[x])));
     }
-    bool join(int a, int b) {
-        a = find(a), b = find(b);
-        if (a == b) return false;
-        if (p[a] > p[b]) swap(a, b);
-        p[a] += p[b];
-        p[b] = a;
-        return true;
+
+    bool join(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            if (sz[y] > sz[x]) {
+                swap(x, y);
+            }
+            p[y] = x;
+            sz[x] += sz[y];
+            return true;
+        }
+        return false;
     }
 };
 
@@ -27,17 +37,17 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, m;
-    cin >> n >> m;
-
-    DSU dsu(n);
-    int comps = n, mx = 1;
-    for (int i = 0; i < m; i++) {
-        int a, b;
-        cin >> a >> b;
-        a--, b--;
-        if (dsu.join(a, b)) comps--;
-        mx = max(mx, max(dsu.size(a), dsu.size(b)));
-        cout << comps << " " << mx << "\n";
+    int n, q;
+    cin >> n >> q;
+    
+    dsu DSU(n);
+    while (q--) {
+        int t, u, v;
+        cin >> t >> u >> v;
+        if (t == 1) {
+            cout << (DSU.find(u) == DSU.find(v)) << '\n';
+        } else {
+            DSU.join(u, v);
+        }
     }
 }
